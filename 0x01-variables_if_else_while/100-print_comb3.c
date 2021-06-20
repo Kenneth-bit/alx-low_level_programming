0007317e7f1f628066246d18f0240cb2ebd73f2d
Kenneth Mpumelelo Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-06-20 15:52:56 -0700

    Create 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * description - Prints all base 10 single digits
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+	int dig;
+
+	for (num = 48; num <= 56; num++)
+	{
+		for (dig = 48; dig <= 57; dig++)
+		{
+			if (num > dig)
+			{
+				putchar(num);
+				putchar(dig);
+
+				if (num != 56 || dig != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
