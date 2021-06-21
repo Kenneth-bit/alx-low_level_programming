124245b4b4c38c49e4a94494d6528bea5d294fa1
Kenneth Mpumelelo Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-06-21 14:04:32 -0700

    Create 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char quote[] = "\"and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	int c;
+
+	for (c=0; c <= 58; c++)
+	{
+		putchar(quote[c]);
+	}
+	putchar('\n');
+	return (0);
+}
