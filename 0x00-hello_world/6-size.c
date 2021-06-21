db1c17b9b11151fb0bb68a2e7291651c76ed18ce
Kenneth Mpumelelo Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-06-21 11:14:10 -0700

    Update 6-size.c, remove extra spaces and add void to main

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,22 @@
-#include<stdio.h>
-int main() {
-    int intType;
-    float floatType;
-    long int longType;
-    long long int longlongType;
-    char charType;
-   
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int intType;
+	float floatType;
+	long int longType;
+	long long int longlongType;
+	char charType;
 
-    // sizeof evaluates the size of a variable
-    printf("Size of a char: %zu byte(s)\n", sizeof(charType));
-    printf("Size of a int: %zu byte(s)\n", sizeof(intType));
-    printf("Size of a long int: %zu byte(s)\n", sizeof(longType));
-    printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongType));
-    printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
-    
-    return (0);
+	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(longType));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongType));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+
+	return (0);
 }
