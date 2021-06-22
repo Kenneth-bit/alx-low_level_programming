fdd6556f5ad7d4c4d6a3a10102a7432fcbee8a88
Kenneth Mpumelelo Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-06-22 03:24:49 -0700

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Always Failure)
  */
 int main(void)
 {
 	char q[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 	int c;
 
-	for (c = 0; c <= 58; c++)
+	for (c = 0; c <= 59; c++)
 	{
 		putchar(q[c]);
 	}
