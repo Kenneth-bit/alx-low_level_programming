5ef3adde32da02ea780e6538886ec94ed8f7c522
Kenneth Mpumelelo Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-06-29 18:38:11 -0700

    Update holberton.h

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,5 +1,4 @@
 #include <unistd.h>
-
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -8,3 +7,8 @@
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c);
+/*
+ * print_alphbet - prints the alphabet in lower case
+ * Return : void
+ */
+void print_alphabet(void);
