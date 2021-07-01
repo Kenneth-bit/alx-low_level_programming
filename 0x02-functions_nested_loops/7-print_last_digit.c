ef75fdc85fe4d15844a9cefb335016ceb63698fe
Kenneth Mpumelelo Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-07-01 04:43:10 -0700

    Create 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - computes the absolute value of int
+ * @n: number to be considered
+ * Return: the last digit of n
+ */
+
+int print_last_digit(int n)
+{
+	int res;
+
+	if (n < 0)
+	{
+		res = (n * -1) % 10;
+	}
+	else if (n > 0)
+	{
+		res = n % 10;
+	}
+	return (res);
+}
