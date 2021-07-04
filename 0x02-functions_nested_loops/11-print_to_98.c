c00c7668de3d34215eb8544b8da07b4500ded16c
Kenneth Mpumelelo Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-07-04 08:43:36 -0700

    Remove double quotes and adjust code

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * print_to_98 - a function that prints all natural numbers from n to 98
- * user input's number prints to 98, regardless < 98 or > 98
+ * user input's number prints to 98
  * @n: number input
  * Return: Always 0 (Success)
  */
@@ -16,7 +16,8 @@ void print_to_98(int n)
 			_putchar(n);
 			if (n != 98)
 			{
-				_putchar(", ");
+				_putchar(',');
+				_putchar(' ');
 			}
 			n++;
 		}
@@ -28,15 +29,17 @@ void print_to_98(int n)
 			_putchar(n);
 			if (n != 98)
 			{
-				_putchar(", ");
+				_putchar(',');
+				_putchar(' ');
 			}
 			n--;
 		}
 	}
 	else
 	{
-		_putchar("98");
+		_putchar('9');
+		_putchar('8');
 	}
-	_putchar("\n");
+	_putchar('\n');
 }
 
