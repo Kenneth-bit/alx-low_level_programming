9f87334f518e12572f31f792f03ecad51269ba22
Kenneth Mpumelelo Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-07-04 08:34:28 -0700

    Add print_to_98(int n) prototype

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -63,3 +63,8 @@ void times_table(void);
   * Return - sum of the two integers
   */
 int add(int, int);
+/**
+  * print_to_98 - prints all natural numbers from n to 98, followed by a new line.
+  * Return - void
+  */
+void print_to_98(int n);
