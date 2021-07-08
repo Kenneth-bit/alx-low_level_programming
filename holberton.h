f5cbd7bf7b3cb32148b4e568236985a36ac9915f
Kenneth Mpumelelo Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-07-08 07:44:05 -0700

    Update holberton.h

--- holberton.h
+++ holberton.h
@@ -3,3 +3,42 @@
   * Return - 1 if c is uppercase, otherwise 0
   */
 int _isupper(int c);
+/**
+  * _isdigit - checks for a digit
+  * Return - 1 if c is is a digit, otherwise 0
+  */
+int _isdigit(int c);
+/**
+  * mul - multiplies two integers
+  * Return - product of the multiplication
+  */
+int mul(int a, int b);
+/**
+  * print_numbers - prints the numbers, 0 to 9
+  */
+void print_numbers(void);
+/**
+  * print_most_numbers - prints 0 - 9 except 2 and 4
+  */
+void print_most_numbers(void);
+/**
+  * more_numbers - prints 10 times the numbers 0 to 14
+  */
+void more_numbers(void);
+/**
+  * print_line - draws a line in the terminal
+  */
+void print_line(int n);
+/**
+  * print_diagonal - draws a diagonal line in the terminal
+  */
+void print_diagonal(int n);
+/**
+  * print_square - prints a square
+  */
+void print_square(int size);
+/**
+  * print_triangel - prints a triangle
+  */
+void print_triangle(int size);
+
