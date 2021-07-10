50a0e5fed52ffaf70c94b0cdfcc3c82b9b09ccd3
Kenneth Mpumelelo Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-07-10 10:00:54 -0700

    Update betty style

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -5,11 +5,14 @@
 
 /**
   * positive_or_negative - prints if integer is postive or negative
+  * @i: integer to be tested
   */
 void positive_or_negative(int i);
 
 /**
   * largest_number - returns largest of 3 numbers
+  * @a, @b, @c: integers to be checked
+  * Return - largest of the three integers
   */
 int largest_number(int a, int b, int c);
 #endif /* HOLBERTON_H */
