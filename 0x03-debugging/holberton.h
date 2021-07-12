623c85effddfed5985801adbdeccae6b551467ac
Kenneth Mpumelelo Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-07-12 13:18:29 -0700

    Add print_remaining days prototype

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -6,6 +6,7 @@
 /**
   * positive_or_negative - prints if integer is postive or negative
   * @i: integer to be tested
+  * Return - void
   */
 void positive_or_negative(int i);
 
@@ -15,4 +16,10 @@ void positive_or_negative(int i);
   * Return - largest of the three integers
   */
 int largest_number(int a, int b, int c);
+/**
+  * print_remaining_days - takes a date and prints how many days are
+  * left in the year, taking leap years into account
+  * Return - void
+  */
+void print_remaining_days(int month, int day, int year)
 #endif /* HOLBERTON_H */
