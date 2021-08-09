e11cacb6cf29314ad38abada166fe6773152abec
Kenneth Mpumelelo Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-08-09 00:58:00 -0700

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - program that prints the first 50 fibonacci numbers
+ * main - program that prints the first 98 fibonacci numbers
  * a: first number
  * b: second number
  * i: Integer
@@ -13,15 +13,18 @@
 int main(void)
 {
 	int i;
-	long a = 1, b = 2, r = 0;
+	float a, b, r;
+	a = 1;
+	b = 2;
+	r= 0;
 
 	printf("1, 2");
-	for (i = 1; i <= 96; i++)
+	for(i = 1; i <= 96; i++)
 	{
 		r = a + b;
 		a = b;
 		b = r;
-		printf(", %ld", r);
+		printf(", %.0f",r);
 	}
 	putchar('\n');
 	return (0);
