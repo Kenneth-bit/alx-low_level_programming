8b4fbcb401e3e7083936230acbec8dc394ee79db
Kenneth Mpumelelo Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-08-09 00:05:08 -0700

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "holberton.h"
+
 /**
- * main - Entry point
+ * main - Write a program that computes and prints the sum of
+ * all the multiples of 3 or 5 below 1024 (excluded)
+ * i: Integer
+ * r: Result
+ * Return: On success 1.
  *
- * Return: Always 0 (Success)
  */
 int main(void)
 {
+	int r = 0, i;
 
+	for (i = 1; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			r = r + i;
+		}
+	}
+	printf("%d\n", r);
 	return (0);
 }
