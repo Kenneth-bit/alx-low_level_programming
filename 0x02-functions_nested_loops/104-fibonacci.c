7f3e35b497af8d389adb5a4f3252e6bd3273a6c1
Kenneth Mpumelelo Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-08-09 01:17:26 -0700

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,15 +13,18 @@
 int main(void)
 {
 	int i;
-	long a = 1, b = 2, r = 0;
+	float a, b, r;
+	a = 1;
+	b = 2;
+	r= 0;
 
 	printf("1, 2");
-	for (i = 1; i <= 96; i++)
+	for(i = 1; i <= 96; i++)
 	{
 		r = a + b;
 		a = b;
 		b = r;
-		printf(", %ld", r);
+		printf(", %.0f",r);
 	}
 	putchar('\n');
 	return (0);
