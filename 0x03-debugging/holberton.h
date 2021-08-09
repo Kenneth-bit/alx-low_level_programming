bb848b5f29cecf26b664d9f41b288f0318762b22
Kenneth Mpumelelo Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-08-09 13:00:36 -0700

    Update hoolberton.h

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -10,18 +10,4 @@
   */
 void positive_or_negative(int i);
 
-/**
-  * largest_number - returns largest of 3 numbers
-  * @a, @b, @c: integers to be checked
-  * Return - largest of the three integers
-  */
-int largest_number(int a, int b, int c);
-/**
-  * print_remaining_days - takes a date and prints how many days are
-  * left in the year, taking leap years into account
-  * Return - void
-  */
-void print_remaining_days(int month, int day, int year);
-int main(void);
-int convert_day(int month, int day);
 #endif /* HOLBERTON_H */
