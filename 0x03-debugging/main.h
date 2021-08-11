074a5986dee431eb0897127288b8cf743c6e0fc6
Kenneth Mpumelelo Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-08-11 03:59:26 -0700

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -11,5 +11,5 @@
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
-void print_largest_number.c(int month, int day);
+void print_largest_number(int month, int day);
 #endif /* HOLBERTON_H */
