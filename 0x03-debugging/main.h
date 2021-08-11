bab6fb0eb78c4be04e3a5fa6ceec8de83991993d
Kenneth Mpumelelo Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-08-11 04:01:56 -0700

    Update 3-print_remaining_days.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,15 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
+include <stdio.h>
 
-/**
-  * positive_or_negative - prints if integer is postive or negative
-  * @i: integer to be tested
-  * Return - void
-  */
 void positive_or_negative(int i);
+
 int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
-void print_largest_number(int month, int day);
+void print_remaining_days(int month, int day, int year);
+
 #endif /* HOLBERTON_H */
