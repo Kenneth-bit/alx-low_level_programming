071945635562fdf651133b47c9b9a4de779e8d5f
Kenneth Mpumelelo Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-08-11 02:32:25 -0700

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,5 +1,5 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 
@@ -9,5 +9,5 @@
   * Return - void
   */
 void positive_or_negative(int i);
-
+int largest_number(int a, int b, int c);
 #endif /* HOLBERTON_H */
