2acd060d995f77a1fc8091cf4cac12757a49b665
Kenneth Mpumelelo Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-08-11 02:16:50 -0700

    Change holberton.h to main.h

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,13 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+
+/**
+  * positive_or_negative - prints if integer is postive or negative
+  * @i: integer to be tested
+  * Return - void
+  */
+void positive_or_negative(int i);
+
+#endif /* HOLBERTON_H */
