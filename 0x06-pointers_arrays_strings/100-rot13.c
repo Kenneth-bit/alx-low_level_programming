bc8b046a3190d30cc9ec5afbe4c1293fb3ae5f90
Kenneth Mpumelelo Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-08-13 05:55:22 -0700

    Create 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,32 +1,27 @@
 #include "main.h"
 
 /**
- * rot13 - Entry point
- * ONE if, TWO loops only...
- * @n: input
- * Return: decrypted string
+ * rot13 - Function that reverses the content of an array of integers.
+ *
+ * @str: s is the array
+ *
+ * Return: Always 0.
  */
-char *rot13(char *n)
+char *rot13(char *str)
 {
-	int x, rot_c = 13, i = 0;
-	char toswap[] = {'A', 'N', 'a', 'n', 'B', 'O', 'b', 'o', 'C', 'P',
-		'c', 'p', 'D', 'Q', 'd', 'q', 'E', 'R', 'e', 'r', 'F', 'S', 'f',
-		's', 'G', 'T', 'g', 't', 'H', 'U', 'h', 'u', 'I', 'V', 'i', 'v',
-		'J', 'W', 'j', 'w', 'K', 'X', 'k', 'x', 'L', 'Y', 'l', 'y', 'M',
-		'Z', 'm', 'z'};
+	int i = 0, k;
+	char s[] =  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char s1[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	while (n[i] != '\0')
+	for (; str[i] != '\0'; i++)
 	{
-		for (x = 0; x <= 51; x++)
+		for (k = 0; k <= 51; k++)
 		{
-			if (n[i] == toswap[x])
+			if (s[k] == str[i])
 			{
-				n[i] = n[i] + rot_c;
-				x = 51;
+				str[i] = s1[k];
 			}
-			rot_c = rot_c * -1;
 		}
-		i++;
 	}
-	return (n);
+	return (str);
 }
