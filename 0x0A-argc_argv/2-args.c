b4f285ecd5eb4fc45317e49a177991d1088164c8
Kenneth Mpumelelo Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-08-14 11:08:49 -0700

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -10,12 +10,12 @@
 
 int main(int argc, char *argv[])
 {
-	int count;
+	int i = 0;
 
-	for (count = 0; count < argc; count++)
+	while (i < argc)
 	{
-		printf("%s\n", argv[count]);
+		printf("%s\n", argv[i++]);
 	}
-
+	
 	return (0);
 }
