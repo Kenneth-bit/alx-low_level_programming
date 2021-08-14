735d96d9a351d96cbe96c7c4462d83f28e85954e
Kenneth Mpumelelo Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-08-14 11:12:39 -0700

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,29 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+
 /**
- * main - Entry Point
- * @argc: arguments
- * @argv: array pointing to arguments
- * Return: 0
+ * main - print product of argument numbers
+ * @argc: argument counter
+ * @argv: numbers to multiply
+ * Return: 0 on success, 1 if two arguments not given
  */
+
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+int j;
+int i = 1;
+int sum = 0;
 
-	if (argc < 1)
-		return (0);
+if (argc == 0)
+{
+printf("0\n");
+return (0);
+}
 
-	for (i = 1; i < argc; i++)
-	{
-		if (!atoi(argv[i]))
-		{
-			printf("%s\n", "Error");
-			return (1);
-		}
-		sum += atoi(argv[i]);
-	}
-	printf("%d\n", sum);
+
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (!(argv[i][j] >= '0' && argv[i][j] <= '9'))
+{
+printf("Error\n");
+return (1);
+}
+}
+sum += atoi(argv[i]);
+}
+
+printf("%d\n", sum);
 
 	return (0);
 }
